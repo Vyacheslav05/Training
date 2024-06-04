eddf18629d64820de8daee39446b6cd4beb2a3a2
Vyacheslav05 <vyacheslav.lobanov@wellpack.org>
2024-06-04 17:30:47 +0300

    Add files via upload

---
+++ training.py
@@ -0,0 +1,7 @@
+name = input('Введите Ваше имя: ')
+current_year = 2024
+date_of_birth = int(input('В каком году вы родились? '))
+age = current_year - date_of_birth
+print('Здравствуйте, ', name)
+print('В этом году Вам ', age, 'лет')
+
